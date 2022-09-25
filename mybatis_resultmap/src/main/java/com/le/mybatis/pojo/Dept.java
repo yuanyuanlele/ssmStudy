5eccef0c7ffa50a6657663b47e416c1ee4d22c0c
yuanyuanlele <1468817353@qq.com>
2022-09-25 19:28:39 +0800

    daka

--- mybatis_resultmap/src/main/java/com/le/mybatis/pojo/Dept.java
+++ mybatis_resultmap/src/main/java/com/le/mybatis/pojo/Dept.java
@@ -1,23 +1,37 @@
 package com.le.mybatis.pojo;
 
+import java.util.List;
+
 public class Dept {
     private Integer deptid;
     private String deptname;
+    private List<Emp> emps;
 
     @Override
     public String toString() {
         return "Dept{" +
                 "deptid=" + deptid +
                 ", deptname='" + deptname + '\'' +
+                ", emps=" + emps +
                 '}';
     }
 
-    public Dept() {
+    public List<Emp> getEmps() {
+        return emps;
     }
 
-    public Dept(Integer deptid, String deptname) {
+    public void setEmps(List<Emp> emps) {
+        this.emps = emps;
+    }
+
+    public Dept(List<Emp> emps) {
+        this.emps = emps;
+    }
+
+    public Dept(Integer deptid, String deptname, List<Emp> emps) {
         this.deptid = deptid;
         this.deptname = deptname;
+        this.emps = emps;
     }
 
     public Integer getDeptid() {
