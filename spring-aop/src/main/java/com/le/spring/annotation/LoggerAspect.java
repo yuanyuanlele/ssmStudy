e07b9cb82fe816623195c8779d43f17a56b73e79
yuanyuanlele <1468817353@qq.com>
2022-10-27 20:22:19 +0800

    jixu xuexi

--- spring-aop/src/main/java/com/le/spring/annotation/LoggerAspect.java
+++ spring-aop/src/main/java/com/le/spring/annotation/LoggerAspect.java
@@ -1,14 +1,28 @@
 package com.le.spring.annotation;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 @Aspect
 public class LoggerAspect {
-    @Before("execution(public int com.le.spring.annotation.CalculatorImpl.add(int,int))")
-    public void beforeAdviceMethod(){
-        System.out.println("loggeraspect,前置通知");
+
+    @Pointcut("execution(* com.le.spring.annotation.CalculatorImpl.*(..))")
+    public void pointcut(){}
+
+    //@Before("execution(public int com.le.spring.annotation.CalculatorImpl.add(int,int))")
+    @Before("pointcut()")
+    public void beforeAdviceMethod(JoinPoint joinPoint){
+        //获取连接点所对应方法的签名信息
+        Signature signature=joinPoint.getSignature();
+        //获取连接点所对应方法的参数
+        Object[] args=joinPoint.getArgs();
+        System.out.println("loggeraspect,方法："+signature.getName()+"参数："+ Arrays.toString(args));
     }
 }
