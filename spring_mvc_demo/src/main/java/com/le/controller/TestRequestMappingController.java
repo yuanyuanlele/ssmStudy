1e19821e6160c3d21e66f7a3e4ca0b60bd4b5966
yuanyuanlele <1468817353@qq.com>
2022-11-15 11:50:21 +0800

    daka

--- spring_mvc_demo/src/main/java/com/le/controller/TestRequestMappingController.java
+++ spring_mvc_demo/src/main/java/com/le/controller/TestRequestMappingController.java
@@ -1,6 +1,7 @@
 package com.le.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,10 +14,16 @@ public class TestRequestMappingController {
     }
     @RequestMapping(
             value = "/hello",
-            method = {RequestMethod.GET,RequestMethod.POST}
+            method = {RequestMethod.GET,RequestMethod.POST},
             headers = {"referer"}
     )
     public String hello(){
+        return "success";
+    }
+
+    @RequestMapping("/test/rest/{id}")
+    public String testRest(@PathVariable("id") Integer id){
+        System.out.println("id:"+id);
         return "success";
     }
 }
