d2b24a305b2091d9cb636cb9232a98e3f9aeddd9
yuanyuanlele <1468817353@qq.com>
2022-11-20 19:00:03 +0800

    1

--- spring_mvc_demo/src/main/java/com/le/controller/testParamController.java
+++ spring_mvc_demo/src/main/java/com/le/controller/testParamController.java
@@ -1,11 +1,14 @@
 package com.le.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class testParamController {
     @RequestMapping(value = "/param")
-    public String getParam(String username,String password){
+    public String getParam(String username, String password, @RequestHeader("referer") String referer,
+                           @CookieValue ("JSESSIONID") String jsessionid){
         System.out.println("username:"+username+"  password"+password+"daka"+"daka");
         return "success";
     }
