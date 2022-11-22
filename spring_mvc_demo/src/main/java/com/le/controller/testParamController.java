a4e55265b3de3705ac6612b5fe35e160ee0cbcab
yuanyuanlele <1468817353@qq.com>
2022-11-22 20:14:46 +0800

    daka

--- spring_mvc_demo/src/main/java/com/le/controller/testParamController.java
+++ spring_mvc_demo/src/main/java/com/le/controller/testParamController.java
@@ -9,7 +9,7 @@ public class testParamController {
     @RequestMapping(value = "/param")
     public String getParam(String username, String password, @RequestHeader("referer") String referer,
                            @CookieValue ("JSESSIONID") String jsessionid){
-        System.out.println("username:"+username+"  password"+password+"daka"+"daka");
+        System.out.println("username:"+username+"  password"+password+"daka"+"daka"+"daka");
         return "success";
     }
 }
