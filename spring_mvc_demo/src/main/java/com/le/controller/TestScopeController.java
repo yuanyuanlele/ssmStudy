3befb22ad8702588da9b403daf25f5f49ba7b871
yuanyuanlele <1468817353@qq.com>
2022-11-24 17:26:17 +0800

    view

--- spring_mvc_demo/src/main/java/com/le/controller/TestScopeController.java
+++ spring_mvc_demo/src/main/java/com/le/controller/TestScopeController.java
@@ -3,6 +3,10 @@ package com.le.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class TestScopeController {
     @RequestMapping("/test/mav")
@@ -11,5 +15,16 @@ public class TestScopeController {
         modelAndView.addObject("testRequestScope","hello,modelandview");
         modelAndView.setViewName("success");
         return modelAndView;
+    }
+    @RequestMapping("/test/session")
+    public String TestSession(HttpSession session){
+        session.setAttribute("testSessionScope","hello,session");
+        return "success";
+    }
+    @RequestMapping("/test/application")
+    public String TestApplication(HttpSession session){
+        ServletContext servletContext=session.getServletContext();
+        servletContext.setAttribute("testApplicationScope","hello,application");
+        return "success";
     }
 }
